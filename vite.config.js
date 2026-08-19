fc66fe8838e9003637e1e78989ff58b7137afa39
Luckyoz1 <luckyoz2003@gmail.com>
2026-08-19 22:32:59 +0200

    remove comment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// base: "./"  -> chemins relatifs, fonctionne sur GitHub Pages
-// (y compris dans un sous-dossier https://user.github.io/mon-depot/).
 export default defineConfig({
   base: "./",
   plugins: [vue()],
